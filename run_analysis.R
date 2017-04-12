7d60927eb70e8d817d7a4e2c1e1688fffd324e90
Julius <medicjue@gmail.com>
2017-04-12 23:25:19 +0800

    Add 1st version of md files

--- run_analysis.R
+++ run_analysis.R
@@ -6,13 +6,13 @@
 # 4. Appropriately labels the data set with descriptive variable names.
 # 5. From the data set in step 4, creates a second, independent tidy data set with the average of each variable for each activity and each subject.
 
+if( !file.exists("UCI HAR Dataset") ) {
+  download.file("https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip",destfile = "data.zip")
+  unzip("data.zip")
+}
 
-
-download.file("https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip",destfile = "data.zip")
-unzip("data.zip")
-
-data.features <- read.table("UCI HAR Dataset/features.txt", header = F , sep = " ")
-data.activity.labels <- read.table("UCI HAR Dataset/activity_labels.txt", header = F , sep = " ")
+data.features <- read.table("UCI HAR Dataset/features.txt", header = F, sep = " ")
+data.activity.labels <- read.table("UCI HAR Dataset/activity_labels.txt", header = F, sep = " ")
 
 ## Load Train Data
 data.train.x <- read.table("UCI HAR Dataset/train/X_train.txt")
@@ -25,24 +25,23 @@ data.test.y <- read.table("UCI HAR Dataset/test/y_test.txt")
 data.test.subject <- read.table("UCI HAR Dataset/test/subject_test.txt")
 
 ## Combine Train and Test
-data.x <- rbind(data.train.x,data.test.x)
+data.x <- rbind(data.train.x, data.test.x)
 data.y <- rbind(data.train.y, data.test.y)
 data.subject <- rbind(data.train.subject, data.test.subject)
 
 features.mean.std <- grepl("mean\\(\\)|std\\(\\)",data.features[,2])
 
 data.extract.x <- data.x[,features.mean.std]
-data.features[features.mean.std,2]
-class(data.extract.x)
 names(data.extract.x) <- data.features[features.mean.std,2]
 
-data.activity.labels
 
 names(data.y) <- "activityId"
 names(data.activity.labels) <- c("activityId","activity")
 
-data <- cbind(data.subject,merge(data.y, data.activity.labels, by.x = "activityId", by.y = "activityId")[,2],data.extract.x)
+data.y.named <- merge(data.y, data.activity.labels, by.x = "activityId", by.y = "activityId")[, 2]
+
+data <- cbind(data.subject, data.y.named, data.extract.x)
 names(data)[1] <- "subject"
 names(data)[2] <- "activity"
 
-write.table(data,"tidy_data.txt", row.names = F)
+write.table(data, "tidy_data.txt", row.names = F)
